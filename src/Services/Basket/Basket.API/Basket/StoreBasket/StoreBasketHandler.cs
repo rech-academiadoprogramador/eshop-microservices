9cebd2b2184079a20948203a8325750cc36cfaf4
rech-academiadoprogramador <rech@academiadoprogramador.net>
2024-06-28 00:10:20 -0300

    Implementado a primeira parte do ms de ordering

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,4 @@
-﻿//using Discount.Grpc;
+﻿using Discount.Grpc;
 
 namespace Basket.API.Basket.StoreBasket;
 
@@ -15,12 +15,12 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 }
 
 public class StoreBasketCommandHandler
-    (IBasketRepository repository /*DiscountProtoService.DiscountProtoServiceClient discountProto*/)
+    (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        //await DeductDiscount(command.Cart, cancellationToken);
+        await DeductDiscount(command.Cart, cancellationToken);
 
         ShoppingCart cart = command.Cart;
         
@@ -34,8 +34,8 @@ public class StoreBasketCommandHandler
         // Communicate with Discount.Grpc and calculate lastest prices of products into sc
         foreach (var item in cart.Items)
         {
-            //var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
-            //item.Price -= coupon.Amount;
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
         }
     }
 }
